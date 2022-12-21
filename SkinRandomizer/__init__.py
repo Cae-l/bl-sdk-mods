dde8cc142f096479b7a6aa989dcbc377adc85158
Cae-l <115944099+Cae-l@users.noreply.github.com>
2022-12-21 17:41:39 -0600

    Update __init__.py

--- SkinRandomizer/__init__.py
+++ SkinRandomizer/__init__.py
@@ -25,14 +25,44 @@ class skinRando(SDKMod):
                         "<font size='12'>2.Turn on/off character customization eligibility</font>\n"
                         "<font size='12'>[Allows Maya heads/skins on Zer0, etc]</font>\n")
  
-    Options: List[ModMenu.Options.Base] = [
-        allSkins
-    ]
-
+    Options: List[ModMenu.Options.Base] = [allSkins]
     SaveEnabledState = EnabledSaveType.LoadOnMainMenu
-
     def Enable(self) -> None:
         super().Enable()
+        global charList
+        charList =  [FindObject("Object","GD_Soldier.Character.CharClass_Soldier"),
+                    FindObject("Object","GD_Assassin.Character.CharClass_Assassin"),
+                    FindObject("Object","GD_Siren.Character.CharClass_Siren"),
+                    FindObject("Object","GD_Mercenary.Character.CharClass_Mercenary"),
+                    FindObject("Object","GD_Tulip_Mechromancer.Character.CharClass_Mechromancer"),
+                    FindObject("Object","GD_Lilac_PlayerClass.Character.CharClass_LilacPlayerClass"),
+                    FindObject("Object","GD_SagePackageDef.Vehicles.VSSUI_ShockFanBoat"),
+                    FindObject("Object","GD_SagePackageDef.Vehicles.VSSUI_IncendiaryFanBoat"),
+                    FindObject("Object","GD_SagePackageDef.Vehicles.VSSUI_CorrosiveFanBoat"),
+                    FindObject("Object","GD_OrchidPackageDef.Vehicles.VSSUI_SawBladeHovercraft"),
+                    FindObject("Object","GD_OrchidPackageDef.Vehicles.VSSUI_RocketHovercraft"),
+                    FindObject("Object","GD_OrchidPackageDef.Vehicles.VSSUI_HarpoonHovercraft"),
+                    FindObject("Object","GD_Globals.VehicleSpawnStation.VSSUI_MGRunner"),
+                    FindObject("Object","GD_Globals.VehicleSpawnStation.VSSUI_RocketRunner"),
+                    FindObject("Object","GD_Globals.VehicleSpawnStation.VSSUI_SawBladeTechnical"),
+                    FindObject("Object","GD_Globals.VehicleSpawnStation.VSSUI_CatapultTechnical")]
+        global charListValues
+        charListValues = [FindClass("CustomizationUsage_Soldier"),
+                        FindClass("CustomizationUsage_Assassin"),
+                        FindClass("CustomizationUsage_Siren"),
+                        FindClass("CustomizationUsage_Mercenary"),
+                        FindClass("CustomizationUsage_ExtraPlayerA"),
+                        FindClass("CustomizationUsage_ExtraPlayerB"),
+                        FindClass("CustomizationUsage_FanBoat"),
+                        FindClass("CustomizationUsage_FanBoat"),
+                        FindClass("CustomizationUsage_FanBoat"),
+                        FindClass("CustomizationUsage_Hovercraft"),
+                        FindClass("CustomizationUsage_Hovercraft"),
+                        FindClass("CustomizationUsage_Hovercraft"),
+                        FindClass("CustomizationUsage_Runner"),
+                        FindClass("CustomizationUsage_Runner"),
+                        FindClass("CustomizationUsage_BanditTech"),
+                        FindClass("CustomizationUsage_BanditTech")]
         if allSkins.CurrentValue == "All Skins":
             self.enableAllSkins()
 
@@ -49,32 +79,14 @@ class skinRando(SDKMod):
 
     def enableAllSkins(self) -> None:
         PC = unrealsdk.GetEngine().GamePlayers[0].Actor
-        PC.ConsoleCommand("set GD_Soldier.Character.CharClass_Soldier EligibleUsage (0)")
-        PC.ConsoleCommand("set GD_Assassin.Character.CharClass_Assassin EligibleUsage (0)")
-        PC.ConsoleCommand("set GD_Siren.Character.CharClass_Siren EligibleUsage (0)")
-        PC.ConsoleCommand("set GD_Mercenary.Character.CharClass_Mercenary EligibleUsage (0)")
-        PC.ConsoleCommand("set GD_Tulip_Mechromancer.Character.CharClass_Mechromancer EligibleUsage (0)")
-        PC.ConsoleCommand("set GD_Lilac_PlayerClass.Character.CharClass_LilacPlayerClass EligibleUsage (0)")
-        PC.ConsoleCommand("set VSSUIDefinition EligibleUsage (0)")
+
+        for char in charList:
+            char.EligibleUsage = [None]
 
     def disableAllSkins(self) -> None:
         PC = unrealsdk.GetEngine().GamePlayers[0].Actor
-        PC.ConsoleCommand("set GD_Soldier.Character.CharClass_Soldier EligibleUsage (Class'WillowGame.CustomizationUsage_Soldier')")
-        PC.ConsoleCommand("set GD_Assassin.Character.CharClass_Assassin EligibleUsage (Class'WillowGame.CustomizationUsage_Assassin')")
-        PC.ConsoleCommand("set GD_Siren.Character.CharClass_Siren EligibleUsage (Class'WillowGame.CustomizationUsage_Siren')")
-        PC.ConsoleCommand("set GD_Mercenary.Character.CharClass_Mercenary EligibleUsage (Class'WillowGame.CustomizationUsage_Mercenary')")
-        PC.ConsoleCommand("set GD_Tulip_Mechromancer.Character.CharClass_Mechromancer EligibleUsage (Class'WillowGame.CustomizationUsage_ExtraPlayerA')")
-        PC.ConsoleCommand("set GD_Lilac_PlayerClass.Character.CharClass_LilacPlayerClass EligibleUsage (Class'WillowGame.CustomizationUsage_ExtraPlayerB')")
-        PC.ConsoleCommand("set GD_SagePackageDef.Vehicles.VSSUI_ShockFanBoat EligibleUsage (Class'WillowGame.CustomizationUsage_FanBoat')")
-        PC.ConsoleCommand("set GD_SagePackageDef.Vehicles.VSSUI_IncendiaryFanBoat EligibleUsage (Class'WillowGame.CustomizationUsage_FanBoat')")
-        PC.ConsoleCommand("set GD_SagePackageDef.Vehicles.VSSUI_CorrosiveFanBoat EligibleUsage (Class'WillowGame.CustomizationUsage_FanBoat')")
-        PC.ConsoleCommand("set GD_OrchidPackageDef.Vehicles.VSSUI_SawBladeHovercraft EligibleUsage (Class'WillowGame.CustomizationUsage_Hovercraft')")
-        PC.ConsoleCommand("set GD_OrchidPackageDef.Vehicles.VSSUI_RocketHovercraft EligibleUsage (Class'WillowGame.CustomizationUsage_Hovercraft')")
-        PC.ConsoleCommand("set GD_OrchidPackageDef.Vehicles.VSSUI_HarpoonHovercraft EligibleUsage (Class'WillowGame.CustomizationUsage_Hovercraft')")
-        PC.ConsoleCommand("set GD_Globals.VehicleSpawnStation.VSSUI_SawBladeTechnical EligibleUsage (Class'WillowGame.CustomizationUsage_BanditTech')")
-        PC.ConsoleCommand("set GD_Globals.VehicleSpawnStation.VSSUI_RocketRunner EligibleUsage (Class'WillowGame.CustomizationUsage_Runner')")
-        PC.ConsoleCommand("set GD_Globals.VehicleSpawnStation.VSSUI_MGRunner EligibleUsage (Class'WillowGame.CustomizationUsage_Runner')")
-        PC.ConsoleCommand("set GD_Globals.VehicleSpawnStation.VSSUI_CatapultTechnical EligibleUsage (Class'WillowGame.CustomizationUsage_BanditTech')")
+        for char, value in zip(charList, charListValues):
+            char.EligibleUsage = [value]
 
     @Hook("WillowGame.CustomizationGFxMovie.MainInputKey")
     def MainInputKey(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
@@ -84,7 +96,6 @@ class skinRando(SDKMod):
             caller.EquippedHeadCustomization = choiceHead
             caller.UpdateHeadPreview(choiceHead)
             caller.CommitHeadCustomization(choiceHead)
-
 
             _skins: List[UObject] = list(caller.SkinCustomizations)
             choiceSkin: UObject = random.choice(_skins)
